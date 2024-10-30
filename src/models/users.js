3170550f94538b621f34d5cfd408cf992ce23677
fsfuseini <suhuyini86@gmail.com>
2024-10-30 05:57:57 +0000

    created a few folders and files

---
+++ src/models/users.js
@@ -0,0 +1,2 @@
+// Register user
+// Login User
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
 // Import libraries
 import express from "express";
+import cors from "cors"
+import mongoose from "mongoose";
+
 
 // Create an express app
 const app = express();
 
+// Connect to the database
+await mongoose.connect(process.env.MONGO_URI);
 
 // Use middlewares
 app.use(express.json());
+app.use(cors);
 
 // Use routes
 
